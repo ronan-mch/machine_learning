58b65f58bbd200fea2cbeccaaa61ba4ab1c739fb
Ronan McHugh <mchugh.r@gmail.com>
2016-10-06 23:04:32 +0200

    Adds function based API and new method to extract titles

--- titanic_data.py
+++ titanic_data.py
@@ -1,10 +1,18 @@
 
 import pandas
 
-with open('titanic_train.csv') as f:
-    original = pandas.read_csv(f)
+def original():
+    with open('titanic_train.csv') as f:
+        return pandas.read_csv(f)
 
-with open('titanic_processed.csv') as f:
-   processed = pandas.read_csv(f)
-   processed = processed.drop(processed.columns[0], 1)
+def processed():
+    with open('titanic_processed.csv') as f:
+       processed = pandas.read_csv(f)
+       return processed.drop(processed.columns[0], 1)
+
+def engineered():
+    data = original()
+    titles = data.Name.str.extract('(Mr|Mrs|Miss|Master|Don|Captain|Col|Rev|Ms|Mme|Dr|Major|Countess|Capt|Mlle|Jonkheer)', expand=False)
+    titles.name = 'Title'
+    return data.join(titles)
 
