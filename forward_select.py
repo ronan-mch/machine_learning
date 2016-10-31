408ff5ac80eda353cb073167eb5ef66944ac0665
Ronan McHugh <mchugh.r@gmail.com>
2016-10-31 22:19:24 +0100

    displays two methods in single subplot

--- forward_select.py
+++ forward_select.py
@@ -28,11 +28,11 @@ def test_kfold(features):
         total_score += score
     return total_score / 10
 
-def compare_features(baseline=None, cur_model=[], try_features=[]):
+def compare_features(test_func, baseline=None, cur_model=[], try_features=[]):
     scores = []
     for feature in try_features:
         try_model = cur_model + [feature]
-        score = test_kfold(try_model)
+        score = test_func(try_model)
         items = [feature, score]
         scores.append(items)
 
@@ -46,20 +46,20 @@ def compare_features(baseline=None, cur_model=[], try_features=[]):
     return best_feature, best_score
 
 
-def forward_select(current_model, remaining_features, baseline, scores=[]):
+def forward_select(current_model, remaining_features, baseline, test_func, scores=[]):
     if len(remaining_features) == 0:
         print "best model is {0}".format(current_model)
-        return current_model, scores
+        return scores
     else:
-        bf, bs = compare_features(baseline=baseline, cur_model=current_model,
+        bf, bs = compare_features(test_func, baseline=baseline, cur_model=current_model,
                 try_features=remaining_features)
         if bf == None: 
             print "New features could not improve on baseline, returning"
-            return current_model, scores
+            return scores
         improved_model = current_model + [bf]
         features_left = remaining_features.drop(bf)
         scores.append([current_model, bs])
-        return forward_select(improved_model, features_left, bs, scores=scores)
+        return forward_select(improved_model, features_left, bs, test_func, scores=scores)
 
 def table_display(labels, scores):
     last_val = 0
@@ -68,15 +68,10 @@ def table_display(labels, scores):
         print "{0} | {1} | {2}".format(labels[i], scores[i], difference)
         last_val = scores[i]
 
-def plot_scores(labels, scores, title):
-    plot.plot(scores)
-    plot.title(title)
-    label_indexes = map(lambda x: x + 1, range(len(labels)))
-    plot.xticks(range(len(labels)), label_indexes)
-    plot.grid(True)
-    plot.yticks()
-    plot.xlabel("Model index")
-    plot.ylabel("Generalisation error")
+def plot_scores(axis, scores, title):
+    axis.plot(scores)
+    axis.set_title(title)
+    axis.grid(True)
 
 def process_scorechart(scorechart):
     label_arrays,scores = zip(*scorechart) 
@@ -95,12 +90,24 @@ data = titanic_data.linear()
 root_baseline = get_baseline(data)
 predictor_features = data.columns.drop('CleanedFare')
 
+# Holdout
 model = []
-selected_model, scorechart = forward_select(model, predictor_features, root_baseline, [])
-labels, scores, error_rates = process_scorechart(scorechart)
-table_display(labels, error_rates)
-#plot_scores(labels, scores)
-plot_scores(labels, error_rates, "Estimated generalisation error - KFold")
+ho_scorechart = forward_select(model, predictor_features, root_baseline, test_holdout, [])
+ho_labels, ho_scores, ho_error_rates = process_scorechart(ho_scorechart)
+table_display(ho_labels, ho_error_rates)
+
+# K-Fold
+model = []
+k_scorechart = forward_select(model, predictor_features, root_baseline, test_kfold, [])
+k_labels, k_scores, k_error_rates = process_scorechart(k_scorechart)
+table_display(k_labels, k_error_rates)
+
+f, axarr = plot.subplots(1, 2, sharey=True)
+plot_scores(axarr[0], ho_error_rates, "Holdout method")
+plot_scores(axarr[1], k_error_rates, "K-Fold method")
+
+f.text(0.5, 0.04, 'Model index', ha='center', va='center')
+f.text(0.06, 0.5, 'Generalisation error', ha='center', va='center', rotation='vertical')
 
 
 plot.show()
